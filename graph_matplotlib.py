cbecae42699b8570b349937e9f774af18250b630
Nathan McLaughlin <129904249+nathanMcL@users.noreply.github.com>
2023-09-10 10:17:57 -0700

    Update graph_matplotlib.py
    # The purpose of this program:
    # Plot given positive and negative values
    # Create a line graph for the positive and negative values
    # Assigned dates to plotted points
    # Created off/on toggle for the line graphs click the [x]
    # Added a notification once lines are toggled
    # Created a hover text tooltip that displays current data

--- graph_matplotlib.py
+++ graph_matplotlib.py
@@ -2,10 +2,12 @@
 # Plot given positive and negative values
 # Create a line graph for the positive and negative values
 # Assigned dates to plotted points
-# Created an off/on toggle for the line graphs click the [x]
+# Created off/on toggle for the line graphs click the [x]
+# Added a notification once lines are toggled
+# Created a hover text tooltip that displays current data
 
 import matplotlib.pyplot as plt
-from matplotlib.widgets import CheckButtons
+from matplotlib.widgets import CheckButtons, Button
 
 # Data
 values = [3.46, 4.23, 5.12, 6.45, 7.67, 8.76]
@@ -44,6 +46,9 @@ ax.set_title('Matplotlib Graph with Toggle Buttons')
 # Add legend
 ax.legend()
 
+# Add grid lines
+ax.grid(True)
+
 
 # Function to toggle lines when check buttons are clicked
 def toggle_lines(label):
@@ -51,6 +56,7 @@ def toggle_lines(label):
         line1.set_visible(not line1.get_visible())
     elif label == 'Negative Line':
         line2.set_visible(not line2.get_visible())
+    ax.set_title(f"{label} Toggled")
     plt.draw()
 
 
@@ -62,5 +68,33 @@ check = CheckButtons(rax, labels, [True, True])
 # Register the toggle function
 check.on_clicked(toggle_lines)
 
-# Show the plot
+tooltip = ax.annotate(
+    "", xy=(0, 0), xytext=(20, 20),
+    textcoords="offset points",
+    bbox=dict(boxstyle="round", fc="w"),
+    arrowprops=dict(arrowstyle="->")
+)
+tooltip.set_visible(False)
+
+
+def on_hover(event):
+    cont1, ind1 = scatter1.contains(event)
+    cont2, ind2 = scatter2.contains(event)
+    if cont1:
+        ind = ind1["ind"][0]  # Take the first index from the array
+        tooltip.xy = (event.xdata, event.ydata)
+        tooltip.set_text(f"Positive: {values[ind]}")
+        tooltip.set_visible(True)
+    elif cont2:
+        ind = ind2["ind"][0]  # Take the first index from the array
+        tooltip.xy = (event.xdata, event.ydata)
+        tooltip.set_text(f"Negative: {neg_values[ind]}")
+        tooltip.set_visible(True)
+    else:
+        tooltip.set_visible(False)
+    fig.canvas.draw_idle()
+
+
+fig.canvas.mpl_connect('motion_notify_event', on_hover)
+
 plt.show()
